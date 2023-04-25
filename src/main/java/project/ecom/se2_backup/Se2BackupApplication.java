b466ddda38c5495effe615777979ce6f3287dec0
gl4ss <luong.thai02@gmail.com>
2023-04-25 18:34:41 +0700

    Initial commit

---
+++ src/main/java/project/ecom/se2_backup/Se2BackupApplication.java
@@ -0,0 +1,13 @@
+package project.ecom.se2_backup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se2BackupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Se2BackupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/ecom/se2_backup/Se2BackupApplicationTests.java
@@ -0,0 +1,13 @@
+package project.ecom.se2_backup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se2BackupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
